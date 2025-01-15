5083fe86eb9b27f002482ec65eef0d1a514c27f0
30091752 <30091752@brighthorizons.com>
2025-01-15 17:28:58 -0500

    Updated User files and added book record files as well.

--- src/main/java/com/book/store/models/domain/Books.java
+++ src/main/java/com/book/store/models/domain/Books.java
@@ -1,4 +1,50 @@
 package com.book.store.models.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "BookRecord")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Books {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private int id;
+
+    @Column(name = "UserID")
+    private int userID;
+
+    @Column(name = "Title")
+    private String title;
+
+    @Column(name = "Author")
+    private String author;
+
+    @Column(name = "Genre")
+    private String genre;
+
+    @Column(name = "TransactionType")
+    private String transactionType;
+
+    @Column(name = "ReturnDate")
+    private LocalDate returnDate;
+
+    @Column(name = "Price")
+    private float price;
+
+    @Column(name = "RentalFee")
+    private float rentalFee;
+
+
+
+
+
 }
